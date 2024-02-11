213cf17e468c920288deaeff7aa4a3738a666887
EfstathiosNikolaosVlachos <157385473+EfstathiosNikolaosVlachos@users.noreply.github.com>
2024-02-11 23:20:35 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -16,16 +16,6 @@ wget https://zenodo.org/records/10489242/files/params60000.npy
 mv params60000.npy  data/params/params_no_pocket.npy
 
 wait
-## Get Uniclust30 (10-20 minutes depending on bandwidth)
-# 25 Gb download, 87 Gb extracted
-wget http://wwwuser.gwdg.de/~compbiol/uniclust/2018_08/uniclust30_2018_08_hhsuite.tar.gz --no-check-certificate
-mkdir data/uniclust30
-mv uniclust30_2018_08_hhsuite.tar.gz data
-cd data
-tar -zxvf uniclust30_2018_08_hhsuite.tar.gz
-cd ..
-
-wait
 ## Install HHblits (a few minutes)
 git clone https://github.com/soedinglab/hh-suite.git
 mkdir -p hh-suite/build && cd hh-suite/build
